f58e2765811cb128f5e5420681cdb56977430925
Manoel Mateus <mateusmanoel@lampp-it.com.br>
2022-10-26 21:53:51 -0300

    Projeto finalizado

--- dist/js/teste.js
+++ dist/js/teste.js
@@ -1,2 +1,15 @@
 "use strict";
-
+const menu = document.querySelector("#menu");
+const toggleBtn = document.querySelector(".simbol");
+toggleBtn.addEventListener("click", () => {
+    let menuVisible = menu.classList.contains("nav-menu");
+    if (!menuVisible) {
+        menu.classList.add("nav-menu");
+    }
+    else {
+        menu.classList.remove("nav-menu");
+    }
+});
+// menu.classList.add("list-menu2");
+console.log(menu.classList.contains("list-menu"));
+console.log(menu.classList);
